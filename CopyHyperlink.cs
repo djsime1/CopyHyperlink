f3bb27c9d230f1ce327d5983a707dcc0782d01d1
David <dj@dj.je>
2024-07-12 09:47:46 -0500

    Add assembly info & slight code cleanup

--- CopyHyperlink.cs
+++ CopyHyperlink.cs
@@ -1,7 +1,5 @@
 ﻿using HarmonyLib;
 using ResoniteModLoader;
-using System;
-using System.Reflection;
 using FrooxEngine;
 using Elements.Core;
 using FrooxEngine.UIX;
@@ -11,8 +9,8 @@ namespace CopyHyperlink;
 public class CopyHyperlink : ResoniteMod
 {
     public override string Name => "CopyHyperlink";
-    public override string Author => "djsime1";
-    public override string Version => "1.0.0";
+    public override string Author => "djsime1 / Zenuru";
+    public override string Version => "1.0.1";
     public override string Link => "https://github.com/djsime1/CopyHyperlink";
 
     public override void OnEngineInit()
@@ -26,24 +24,17 @@ public class CopyHyperlink : ResoniteMod
     {
         public static void Postfix(HyperlinkOpenDialog __instance, ref SyncRef<Button> ____openButton, Sync<Uri> ___URL)
         {
-            // UniLog.Log($"[CopyHyperlink] Injecting button!\n\t__instance: {__instance}\n\t____openButton: {____openButton}\n\t___URL: {___URL}", true);
-
             var ui = new UIBuilder(____openButton.Target.Slot.Parent);
             RadiantUI_Constants.SetupEditorStyle(ui);
 
             var btn = ui.Button("Interaction.CopyLink".AsLocaleKey(), RadiantUI_Constants.Sub.CYAN);
             var text = btn.Slot.GetComponentInChildren<Text>();
-            btn.LocalPressed += (button, data) => {
+            btn.LocalPressed += (_, _) =>
+            {
                 __instance.InputInterface.Clipboard.SetText(___URL.Value.ToString());
                 text.LocaleContent = "General.CopiedToClipboard".AsLocaleKey();
-                // HighlightHelper.FlashHighlight(__instance.Slot, null, colorX.Cyan);
-                __instance.RunInSeconds(2, delegate
-                {
-                    text.LocaleContent = "Interaction.CopyLink".AsLocaleKey();
-                });
+                __instance.RunInSeconds(2, () => text.LocaleContent = "Interaction.CopyLink".AsLocaleKey());
             };
-
-            // UniLog.Log($"[CopyHyperlink] Done.\n\tui: {ui}\n\tbtn: {btn}");
         }
     }
 }
