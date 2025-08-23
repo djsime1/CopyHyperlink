fc89303eb7978b3f80f823768dee64a18915c477
David <dj@dj.je>
2025-08-23 16:32:49 -0500

    Bumpety bumb bub bump!

--- CopyHyperlink.cs
+++ CopyHyperlink.cs
@@ -10,7 +10,7 @@ public class CopyHyperlink : ResoniteMod
 {
     public override string Name => "CopyHyperlink";
     public override string Author => "djsime1 / Zenuru";
-    public override string Version => "1.0.1";
+    public override string Version => "1.0.2";
     public override string Link => "https://github.com/djsime1/CopyHyperlink";
 
     public override void OnEngineInit()
@@ -24,10 +24,18 @@ public class CopyHyperlink : ResoniteMod
     {
         public static void Postfix(HyperlinkOpenDialog __instance, ref SyncRef<Button> ____openButton, Sync<Uri> ___URL)
         {
+            if (__instance.InputInterface.Clipboard is null)
+            {
+                Warn("InputInterface has no Clipboard, CopyHyperlink can't do anything.");
+                return;
+            }
+
+            ____openButton.Target.Slot.Parent.Children.Last().OrderOffset = 2; // Keep Cancel as last button
             var ui = new UIBuilder(____openButton.Target.Slot.Parent);
             RadiantUI_Constants.SetupEditorStyle(ui);
 
             var btn = ui.Button("Interaction.CopyLink".AsLocaleKey(), RadiantUI_Constants.Sub.CYAN);
+            btn.Enabled = btn.InputInterface.IsClipboardSupported;
             var text = btn.Slot.GetComponentInChildren<Text>();
             btn.LocalPressed += (_, _) =>
             {
